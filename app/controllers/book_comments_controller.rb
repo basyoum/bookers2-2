e5be3e6dbbc32b130582da9720e08f4ebb7f941e
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-06-10 11:40:48 +0000

    [Add]コメント機能非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,21 @@
 class BookCommentsController < ApplicationController
+
   def create
     #ネストしたURLを作成することでparams[:book_id]でBookのidが取得できるようになる
     book = Book.find(params[:book_id])
     #ログインしているユーザに空を渡す
-    comment = current_user.book_comments.new(book_comment_params)
+    @comment = current_user.book_comments.new(book_comment_params)
     #どの投稿にコメントしているか
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
     #コメントのidとBookのidを取得し削除
     # find_byメソッド→モデルの主キー以外の条件でも、レコードを取得することができる
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    #BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
